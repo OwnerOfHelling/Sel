73e1f27bb79ce4b054704a954600a1cbb5c322f7
STINGER <romanov_ruslan@mail.ru>
2020-12-18 15:20:45 +0300

    Final version 2.0

--- src/main/java/steps/StepDef.java
+++ src/main/java/steps/StepDef.java
@@ -16,85 +16,84 @@ import java.util.List;
 
 public class StepDef {
 
-        protected WebDriver driver;
+    protected WebDriver driver;
 
 
-        @Before
-        public void setUp() {
-            System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
-            driver = new ChromeDriver();
-        }
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
 
-        @Пусть("открыт ресурс авито")
-        public void открыт_ресурс_авито() {
-            driver.navigate().to("https://www.avito.ru/");
-        }
+    @Пусть("открыт ресурс авито")
+    public void открыт_ресурс_авито() {
+        driver.navigate().to("https://www.avito.ru/");
+    }
 
-        @И("в выпадающем списке категорий выбрана {word}")
-        public void вВыпадающемСпискеКатегорийВыбранаОргтехника(String Принтер) {
-            Select choice = new Select(driver.findElement(By.xpath("//*[@id='category']")));
-            choice.selectByValue("99");
-        }
+    @И("в выпадающем списке категорий выбрана оргтехника")
+    public void вВыпадающемСпискеКатегорийВыбранаОргтехника() {
+        Select choice = new Select(driver.findElement(By.id("category")));
+        choice.selectByValue("99");
+    }
 
-        @И("в поле поиска введено значение {word}")
-        public void вПолеПоискаВведеноЗначениеПринтер(String Принтер) {
-            driver.findElement(By.xpath("//*[@id='search']")).sendKeys("Принтер");
-        }
+    @И("в поле поиска введено значение {word}")
+    public void вПолеПоискаВведеноЗначениеПринтер(String name) {
+        driver.findElement(By.xpath("//*[@id='search']")).sendKeys(name);
+    }
 
-        @Тогда("кликнуть по выпадающему списку региона")
-        public void кликнутьПоВыпадающемуСпискуРегиона() {
-            WebElement region = driver.findElement(By.xpath("//div[@data-marker='search-form/region']"));
-            region.click();
-        }
+    @Тогда("кликнуть по выпадающему списку региона")
+    public void кликнутьПоВыпадающемуСпискуРегиона() {
+        WebElement region = driver.findElement(By.xpath("//div[@data-marker='search-form/region']"));
+        region.click();
+    }
 
-        @Тогда("в поле регион введено значение {word}")
-        public void вПолеРегионВведеноЗначениеВладивосток(String Владивосток) {
-            driver.findElement(By.xpath("//input[@data-marker='popup-location/region/input']")).sendKeys("Владивосток");
-        }
+    @Тогда("в поле регион введено значение {word}")
+    public void вПолеРегионВведеноЗначениеВладивосток(String city) {
+        driver.findElement(By.xpath("//input[@data-marker='popup-location/region/input']")).sendKeys(city);
+    }
 
-        @И("нажата кнопка показать объявления")
-        public void нажатаКнопкаПоказатьОбъявления() {
-            WebElement suggest0 = driver.findElement(By.xpath("//button[@data-marker='popup-location/save-button']"));
-            suggest0.click();
-        }
+    @И("нажата кнопка показать объявления")
+    public void нажатаКнопкаПоказатьОбъявления() {
+        WebElement suggest0 = driver.findElement(By.xpath("//button[@data-marker='popup-location/save-button']"));
+        suggest0.click();
+    }
 
-        @Тогда("открылась страница результаты по запросу принтер")
-        public void открыласьСтраницаРезультатыПоЗапросуПринтер() {
-            driver.get("https://www.avito.ru/vladivostok/orgtehnika_i_rashodniki?q=%D0%BF%D1%80%D0%B8%D0%BD%D1%82%D0%B5%D1%80");
-            if (driver.getTitle() != null && driver.getTitle().contains("принтер - Купить оргтехнику и расходники в Владивостоке с доставкой: принтер, сканер, МФУ, бумага | Недорогая электроника | Авито")) {
-            } else {
-                System.out.println(driver.getTitle());
-                System.out.println("Открыта не та страница !");
-            }
-        }
-
-        @И("активирован чекбокс только с фотографией")
-        public void активированЧекбоксТолькоСФотографией() {
-            WebElement checkbox = driver.findElement(By.xpath("//label[2]/span"));
-            if (!checkbox.isSelected()) {
-                checkbox.click();
-            }
-        }
-
-        @И("в выпадающем списке сортировка выбрано значение {word}")
-        public void вВыпадающемСпискеСортировкаВыбраноЗначениеДороже(String Дороже) {
-            Select sort = new Select(driver.findElement(By.xpath("//div[2]/select")));
-            sort.selectByValue("2");
-        }
-
-        @И("в консоль выведено значение названия и цены 3 первых товаров")
-        public void вКонсольВыведеноЗначениеНазванияИЦены3ПервыхТоваров() {
-            WebElement catalog = driver.findElement(By.xpath("//div[@data-marker='catalog-serp']"));
-            List<WebElement> price = catalog.findElements(By.xpath("//div[@class='iva-item-body-NPl6W']"));
-            for (int i = 0; i < 3; i++) {
-                System.out.println(price.get(i).getText());
-            }
-        }
-
-        @After
-        public void tearDown() {
+    @Тогда("открылась страница результаты по запросу {word}")
+    public void открыласьСтраницаРезультатыПоЗапросуПринтер(String text) {
+        WebElement searc = driver.findElement(By.xpath("//*[@id='search']"));
+        if (searc != null && searc.equals(text)) {
+            System.out.println("Открыта не та страница !");
             driver.close();
         }
-
     }
+
+    @И("активирован чекбокс только с фотографией")
+    public void активированЧекбоксТолькоСФотографией() {
+        WebElement checkbox = driver.findElement(By.xpath("//label[2]/span"));
+        if (!checkbox.isSelected()) {
+            checkbox.click();
+        }
+    }
+
+    @И("в выпадающем списке сортировка выбрано значение {word}")
+    public void вВыпадающемСпискеСортировкаВыбраноЗначениеДороже(String price) {
+        Select sort = new Select(driver.findElement(By.xpath("//div[2]/select")));
+        sort.selectByVisibleText(price);
+    }
+
+    @И("в консоль выведено значение названия и цены {int} первых товаров")
+    public void вКонсольВыведеноЗначениеНазванияИЦены3ПервыхТоваров(int num) {
+        WebElement catalog = driver.findElement(By.xpath("//div[@data-marker='catalog-serp']"));
+        List<WebElement> price = catalog.findElements(By.xpath("//div[@class='iva-item-body-NPl6W']"));
+        for (int i = 0; i < num; i++) {
+            System.out.println(price.get(i).getText());
+        }
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+}
